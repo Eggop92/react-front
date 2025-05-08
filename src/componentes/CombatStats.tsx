9fc7fe5cc138a90e49e8c83d034cde41232d71df
e.puerta <e.puerta@speexx.com>
2025-05-08 14:02:12 +0200

    Hit points

--- src/componentes/CombatStats.tsx
+++ src/componentes/CombatStats.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "@mui/material"
 import ArmorClass from "./ArmorClass"
 import DeathSaves from "./DeathSaves"
+import HitPoints from "./HitPoints"
 import Initiative from "./Initiative"
 import ProficencyBonus from "./ProficencyBonus"
 
@@ -9,7 +10,7 @@ const CombatStats = () => {
         <Stack spacing={2}>
             <Stack direction="row" spacing={2} sx={{ alignItems: 'center' }}>
                 <ArmorClass armorClass={15} />
-                <div>Hit Points</div>
+                <HitPoints maxHitPoints={20} />
                 <div>Speed</div>
             </Stack>
             <Stack direction="row" spacing={2} sx={{ alignItems: 'flex-start' }}>
---
+++ src/componentes/HitPoints.tsx
@@ -0,0 +1,37 @@
+import { NumberField } from '@base-ui-components/react/number-field';
+import { Chip, Stack } from "@mui/material";
+import { useState } from 'react';
+import { TiHeartFullOutline, TiHeartHalfOutline, TiHeartOutline, TiMinus, TiPlus } from 'react-icons/ti';
+
+interface HitPointsProps {
+    maxHitPoints: number;
+}
+const HitPoints = ({ maxHitPoints }: HitPointsProps) => {
+    const [currentHitPoints, setCurrentHitPoints] = useState<number | null>(maxHitPoints);
+    const onNumberChange = (num: number | null) => {
+        setCurrentHitPoints(num)
+    }
+    return (
+        <Stack direction='column' className="border" padding={1} sx={{ alignItems: 'center' }} spacing={1}>
+            <Chip label="Hit points" color="primary" />
+            <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }} >
+                {currentHitPoints === maxHitPoints ? <TiHeartFullOutline size='1.5rem' /> : (currentHitPoints === 0 ? <TiHeartOutline size='1.5rem' /> : <TiHeartHalfOutline size='1.5rem' />)}
+                <NumberField.Root defaultValue={maxHitPoints} max={maxHitPoints} min={0} className={'number-input Field'} onValueChange={onNumberChange} >
+                    <NumberField.Group className={'number-input Group'}>
+                        <NumberField.Decrement className={'number-input Decrement'}>
+                            <TiMinus />
+                        </NumberField.Decrement>
+                        <NumberField.Input className={'number-input Input'} />
+                        <NumberField.Increment className={'number-input Increment'}>
+                            <TiPlus />
+                        </NumberField.Increment>
+                    </NumberField.Group>
+                </NumberField.Root>
+                {/* <TbBackslash />
+                <Typography variant="h5">{maxHitPoints}</Typography> */}
+            </Stack>
+        </Stack>
+    )
+}
+
+export default HitPoints
