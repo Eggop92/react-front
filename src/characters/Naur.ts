06f94e7b4848d58a064e9b7d739e290fa29efcca
e.puerta <e.puerta@speexx.com>
2025-05-28 12:23:47 +0200

    images issue

--- src/characters/Naur.ts
+++ src/characters/Naur.ts
@@ -6,7 +6,7 @@ export const naur: Character = {
     race: "Tiefling",
     background: "Vagabundo",
     level: 9,
-    imageProfile: '../public/naur.jpg',
+    imageProfile: 'naur.jpg',
     caracteristics: [
         { id: 1, value: 10 },
         { id: 2, value: 16 },
